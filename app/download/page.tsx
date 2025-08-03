c552ac1d619c6347dfbbfd9d2061486a51f71191
Sarina-R <Sarina.r.n.0@gmail.com>
2025-08-03 12:30:46 +0330

    fix: links to google drive

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -41,8 +41,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Mac Universal',
-            size: '145 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_0_Mac_Universal/.zip',
+            size: '184 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_0_Mac_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1535B3IWH4AEZlqtG2vkCbxsYGfSRTRbs/view?usp=drive_link',
           },
         ],
       },
@@ -51,9 +52,16 @@ const versions: Version[] = [
         icon: <FaWindows className='w-4 h-4' />,
         downloads: [
           {
-            name: 'Windows',
-            size: '125 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_0_Win_Universal/.zip',
+            name: 'Windows X64',
+            size: '182 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_0_Win_Universal/.zip',
+            url: 'https://drive.google.com/file/d/12diU6qCgTT_iC_9WiZJn9kLm3fXfehzM/view?usp=drive_link',
+          },
+          {
+            name: 'Windows X86',
+            size: '174 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_0_Win_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1vEqyOi7c5ziSE4ZcEsj_NwqK3PCeZ1g0/view?usp=drive_link',
           },
         ],
       },
@@ -63,53 +71,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Linux Universal',
-            size: '135 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_0_Linux_Universal/.zip',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    version: '1.2.3',
-    isLatest: false,
-    releaseDate: '2025-06-03',
-    platforms: [
-      {
-        name: 'macOS',
-        icon: <FaApple className='w-4 h-4' />,
-        downloads: [
-          {
-            name: 'Mac Universal',
-            size: '146 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_3_Mac_Universal/.zip',
-          },
-        ],
-      },
-      {
-        name: 'Windows',
-        icon: <FaWindows className='w-4 h-4' />,
-        downloads: [
-          {
-            name: 'Windows x64',
-            size: '130 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_3_Win_x64/.zip',
-          },
-          {
-            name: 'Windows x86',
-            size: '125 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_3_Win_x86/.zip',
-          },
-        ],
-      },
-      {
-        name: 'Linux',
-        icon: <FaLinux className='w-4 h-4' />,
-        downloads: [
-          {
-            name: 'Linux Universal',
-            size: '136 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_3_Linux_Universal/.zip',
+            size: '191 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_0_Linux_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1474kpPOf3dcqQcqyIc_-BK5lo2wN-YNp/view?usp=drive_link',
           },
         ],
       },
@@ -126,8 +90,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Mac Universal',
-            size: '147 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_4_Mac_Universal/.zip',
+            size: '189 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_4_Mac_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1Pl3ij4J5squtAFeuxxpftFXOYmGB8dAp/view',
           },
         ],
       },
@@ -137,8 +102,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Windows',
-            size: '126 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_4_Win_Universal/.zip',
+            size: '186 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_4_Win_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1HjHECsspQNi5ggZPoyGr9WE3z3IodwOu/view',
           },
         ],
       },
@@ -148,8 +114,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Linux Universal',
-            size: '137 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_4_Linux_Universal/.zip',
+            size: '195 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_4_Linux_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1gJG9fA3iPqoWPmFmBklpEXVeM7IYAHgc/view?usp=drive_link',
           },
         ],
       },
@@ -161,42 +128,21 @@ const versions: Version[] = [
     releaseDate: '2025-06-10',
     platforms: [
       {
-        name: 'macOS',
-        icon: <FaApple className='w-4 h-4' />,
-        downloads: [
-          {
-            name: 'Mac Universal',
-            size: '148 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_6_Mac_Universal/.zip',
-          },
-        ],
-      },
-      {
         name: 'Windows',
         icon: <FaWindows className='w-4 h-4' />,
         downloads: [
           {
             name: 'Windows',
-            size: '127 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_6_Win_Universal/.zip',
-          },
-        ],
-      },
-      {
-        name: 'Linux',
-        icon: <FaLinux className='w-4 h-4' />,
-        downloads: [
-          {
-            name: 'Linux Universal',
-            size: '138 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_6_Linux_Universal/.zip',
+            size: '192 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_6_Win_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1HeoZrr4L-2_KFwBcCea9CxmxwfUtQgGa/view?usp=drive_link',
           },
         ],
       },
     ],
   },
   {
-    version: '1.2.7 ACL',
+    version: '1.2.7',
     isLatest: false,
     releaseDate: '2025-06-12',
     platforms: [
@@ -206,8 +152,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Mac Universal',
-            size: '149 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Mac_Universal/.zip',
+            size: '251 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Mac_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1cZEZtzgzyDE5vrGai8548JADHhuOn4aW/view?usp=drive_link',
           },
         ],
       },
@@ -217,8 +164,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Windows',
-            size: '128 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Win_Universal/.zip',
+            size: '236 MB',
+            url: 'https://drive.google.com/file/d/1CmSlkFDuqhZh8DHkSbTWCUyQHLgfYNyb/view?usp=drive_link',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Win_Universal/.zip',
           },
         ],
       },
@@ -228,8 +176,95 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Linux Universal',
-            size: '139 MB',
-            url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Linux_Universal/.zip',
+            size: '274 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Linux_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1vuWaMYRvc_EvPbqdNcjsPj4xKwRtV4Yx/view?usp=drive_link',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    version: '1.2.8',
+    isLatest: false,
+    releaseDate: '2025-06-12',
+    platforms: [
+      {
+        name: 'macOS',
+        icon: <FaApple className='w-4 h-4' />,
+        downloads: [
+          {
+            name: 'Mac Universal',
+            size: '140 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Mac_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1RcnLNSD2syq9LDoAc5KTYJ2kYUQ_fbxn/view?usp=drive_link',
+          },
+        ],
+      },
+      {
+        name: 'Windows',
+        icon: <FaWindows className='w-4 h-4' />,
+        downloads: [
+          {
+            name: 'Windows',
+            size: '133 MB',
+            url: 'https://drive.google.com/file/d/1NzEHfmbZgzsbSXPrcQwr6o20yD7tP7L7/view?usp=drive_link',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Win_Universal/.zip',
+          },
+        ],
+      },
+      {
+        name: 'Linux',
+        icon: <FaLinux className='w-4 h-4' />,
+        downloads: [
+          {
+            name: 'Linux Universal',
+            size: '140 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Linux_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1kHisBUxEej87AKcasfarmtlFiNSfW9Zz/view?usp=drive_link',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    version: '1.2.9',
+    isLatest: false,
+    releaseDate: '2025-06-12',
+    platforms: [
+      {
+        name: 'macOS',
+        icon: <FaApple className='w-4 h-4' />,
+        downloads: [
+          {
+            name: 'Mac Universal',
+            size: '140 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Mac_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1vrUs0rAJgcnxZutztJWYamlkzAhv_uBA/view?usp=drive_link',
+          },
+        ],
+      },
+      {
+        name: 'Windows',
+        icon: <FaWindows className='w-4 h-4' />,
+        downloads: [
+          {
+            name: 'Windows',
+            size: '133 MB',
+            url: 'https://drive.google.com/file/d/10X0tIESzn6mrSzNvWqzExFxyukK5ATh1/view?usp=drive_link',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Win_Universal/.zip',
+          },
+        ],
+      },
+      {
+        name: 'Linux',
+        icon: <FaLinux className='w-4 h-4' />,
+        downloads: [
+          {
+            name: 'Linux Universal',
+            size: '140 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_2_7_ACL_Linux_Universal/.zip',
+            url: 'https://drive.google.com/file/d/14MWrg34spkNIENkZSCf6v1qX-0KqqkUF/view?usp=drive_link',
           },
         ],
       },
@@ -246,8 +281,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Windows x64',
-            size: '134 MB',
-            url: 's3.avisengine.com/AVISEngine_1_3_0_Win_x64/.zip',
+            size: '116 MB',
+            // url: 's3.avisengine.com/AVISEngine_1_3_0_Win_x64/.zip',
+            url: 'https://drive.google.com/file/d/1yOp76fIzPhGMzVjSJ8u2UH-8xrQRt-H1/view?usp=drive_link',
           },
         ],
       },
@@ -264,8 +300,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Mac Universal',
-            size: '150 MB',
-            url: 's3.avisengine.com/AVISEngine_2_0_0_Mac_Universal/.zip',
+            size: '238 MB',
+            // url: 's3.avisengine.com/AVISEngine_2_0_0_Mac_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1G3fpNL8Mvi_VsvdyVcVAzRWEnq2pRekv/view?usp=drive_link',
           },
         ],
       },
@@ -275,8 +312,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Windows',
-            size: '130 MB',
-            url: 's3.avisengine.com/AVISEngine_2_0_0_Win_Universal/.zip',
+            size: '229 MB',
+            // url: 's3.avisengine.com/AVISEngine_2_0_0_Win_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1rADQgUSmTk8yx_pQ6dBTcsL46rLkw3Zu/view?usp=drive_link',
           },
         ],
       },
@@ -286,8 +324,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Linux Universal',
-            size: '140 MB',
-            url: 's3.avisengine.com/AVISEngine_2_0_0_Linux_Universal/.zip',
+            size: '242 MB',
+            // url: 's3.avisengine.com/AVISEngine_2_0_0_Linux_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1IjBnfmOJrt86OTh53ZLxqYMpqKkmMOnB/view?usp=drive_link',
           },
         ],
       },
@@ -304,8 +343,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Mac Universal',
-            size: '151 MB',
-            url: 's3.avisengine.com/AVISEngine_2_0_1_Mac_Universal/.zip',
+            size: '238 MB',
+            // url: 's3.avisengine.com/AVISEngine_2_0_1_Mac_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1TDerza3IxHzkxEiSoeoaGjfrIAJtHCRM/view?usp=drive_link',
           },
         ],
       },
@@ -315,8 +355,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Windows',
-            size: '131 MB',
-            url: 's3.avisengine.com/AVISEngine_2_0_1_Win_Universal/.zip',
+            size: '229 MB',
+            // url: 's3.avisengine.com/AVISEngine_2_0_1_Win_Universal/.zip',
+            url: 'https://drive.google.com/file/d/1WRZ627dZj61p6YvWx12BoLdxGW1_jHDl/view?usp=drive_link',
           },
         ],
       },
@@ -326,8 +367,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Linux Universal',
-            size: '141 MB',
-            url: 's3.avisengine.com/AVISEngine_2_0_1_Linux_Universal/.zip',
+            size: '214 MB',
+            // url: 's3.avisengine.com/AVISEngine_2_0_1_Linux_Universal/.zip',
+            url: 'https://drive.google.com/file/d/18qDyM2AlE1X7rfgR0FY1Bm_8Ot4uSiHO/view?usp=drive_link',
           },
         ],
       },
@@ -344,8 +386,9 @@ const versions: Version[] = [
         downloads: [
           {
             name: 'Windows x64',
-            size: '137 MB',
-            url: 's3.avisengine.com/AVISEngine_2_3_1_Win_x64/.zip',
+            size: '329 MB',
+            // url: 's3.avisengine.com/AVISEngine_2_3_1_Win_x64/.zip',
+            url: 'https://drive.google.com/file/d/1-F0yMRl22Ql9XphTTU_c6kDTX6fLQpbQ/view?usp=drive_link',
           },
         ],
       },
@@ -544,6 +587,9 @@ const CursorDownloadPage: React.FC = () => {
                                 <motion.div
                                   key={index}
                                   className='flex items-center justify-between p-2  border-neutral-800 rounded hover:bg-neutral-900 cursor-pointer text-xs'
+                                  onClick={() =>
+                                    window.open(download.url, '_blank')
+                                  }
                                   transition={{ duration: 0.2 }}
                                 >
                                   <div>
